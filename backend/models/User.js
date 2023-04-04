f7fbbe4e06dbaa49a17be078780c4112c9d9bb72
Marko Ivezic <markoivezic89@gmail.com>
2023-04-04 12:38:50 +0200

    added models, middleware and routes

---
+++ backend/models/User.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    picture: {
+      type: String,
+      default: "../public/assets/profilePhoto.png",
+    },
+    location: String,
+    occupation: String,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
