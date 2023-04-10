c93006a5cc6233531d552046b0a8c4d84573e84a
Marko Ivezic <markoivezic89@gmail.com>
2023-04-10 14:36:11 +0200

    registration and login page

---
+++ frontend/src/scene/homePage/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div><h1 className='font-thin text-xs' >HomePage</h1></div>
+  )
+}
+
+export default HomePage
---
+++ frontend/src/scene/registerPage/index.jsx
@@ -0,0 +1,133 @@
+import { LockClosedIcon } from "@heroicons/react/20/solid";
+import "../../App.css";
+
+function Register({ setLogIn }) {
+  return (
+    <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Register your new account
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className=" -space-y-px rounded-md shadow-sm">
+            <div className="py-2">
+              <label htmlFor="first-name" className="sr-only">
+                First name
+              </label>
+              <input
+                id="first-name"
+                name="first name"
+                type="first name"
+                autoComplete="first name"
+                required
+                className="relative block w-full rounded border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="First name"
+              />
+            </div>
+            <div className="py-2">
+              <label htmlFor="last-name" className="sr-only">
+                Last name
+              </label>
+              <input
+                id="last-name"
+                name="last name"
+                type="last name"
+                autoComplete="last name"
+                required
+                className="relative block w-full rounded border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Last name"
+              />
+            </div>
+            <div className="py-2">
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="relative block w-full rounded border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Email address"
+              />
+            </div>
+            <div className="py-2">
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="relative block w-full rounded border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="group relative flex w-40 justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <LockClosedIcon
+                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  aria-hidden="true"
+                />
+              </span>
+              Register
+            </button>
+          </div>
+          <p className="mt-2 text-center text-3xl text-gray-600">
+            <p className="text-sm">Already have an account?</p>
+            <p
+              onClick={() => setLogIn(true)}
+              className="text-sm text-indigo-600 hover:text-indigo-500 cursor-pointer"
+            >
+              Sign in
+            </p>
+          </p>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+              />
+              <label
+                htmlFor="remember-me"
+                className="ml-2 block text-sm text-gray-900"
+              >
+                Remember me
+              </label>
+            </div>
+
+            <div className="text-sm">
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
