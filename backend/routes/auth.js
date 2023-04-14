df3fedc62011a8d4a82ac9882a931884febd4c9a
Marko Ivezic <markoivezic89@gmail.com>
2023-04-14 10:52:56 +0200

    Added verify route to verify token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,14 @@
 import express from "express";
 import {login, register} from "../controllers/auth.js";
+import verifyToken from "../middleware/auth.js"
 
 const router = express.Router();
 
 router.post("/register", register)
 router.post("/login", login );
+router.get('/verify', verifyToken, (req, res) => {
+    console.log(`req.payload`, req.payload);
+    res.status(200).json(req.payload);
+});
 
 export default router;
